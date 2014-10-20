821827d1ceec24624281de2134c3f3d9adc70e8c
litigoner <erbijayamaharjan@gmail.com>
2014-10-20 14:42:30 +0000

    Create index.php

---
+++ cleanupsol/index.php
@@ -0,0 +1,7 @@
+<h4>please enter the file you want to process:</h4></br>
+<form method="post" enctype="multipart/form-data" action="process.php">
+    awards file = <input type='file' name="unorg1"/><br>
+    contracts file = <input type='file' name="unorg2"/><br>
+    </br>
+    <input type='submit' value="submit"/></br>
+</form>
